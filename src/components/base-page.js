benjamts/wedsite

commit 52e69bb71fab334743ca904cda579d889640562e

Make Image Paths Absolute

src/components/base-page.js
@@ -32,7 +32,7 @@ class BasePage extends React.Component {
     */
     const preloadImg = new window.Image();
     preloadImg.addEventListener('load', () => {
-      preloadedBGImg = largeBackground;
+      preloadedBGImg = `/${largeBackground}`;
       this.setState({ preloaded: true });
     });
     preloadImg.src = `/${largeBackground}`;

src/components/bridal-party-page.js
@@ -77,7 +77,7 @@ class PartyMember extends React.PureComponent {
           <img
             alt={`Picture of ${this.props.name}`}
             className={styles.photo}
-            src={this.props.imgSrc}
+            src={`/${this.props.imgSrc}`}
           />
         </span>
         <h2 className={styles.name}>{this.props.name}</h2>

src/components/home-page.js
@@ -24,7 +24,7 @@ export default (props) => {
       </p>
       <img
         className={styles.cutePhoto}
-        src={cutePhoto}
+        src={`/${cutePhoto}`}
       />
 
       <h2 className={styles.sectionHeader}>Our Story</h2>

src/components/registry-page.js
@@ -40,7 +40,7 @@ class Charity extends React.PureComponent {
         <a className={styles.link}>
           <img
             className={styles.logo}
-            src={this.props.logoSrc}
+            src={`/${this.props.logoSrc}`}
           />
           <h3 className={styles.name}>{this.props.name}</h3>
           <p className={styles.description}>{this.props.description}</p>

commit c904d1a4a4ff4c1e972440fc305082218ddfb094

Prevent Background Blur From Being Re-Applied On Page Change
Making image paths absolute in 52e69bb71fab334743ca904cda579d889640562e
broke an equality check that prevented the blur effect from being
reapplied on page change.

Two birds, one commit.

src/components/base-page.js
@@ -1,6 +1,6 @@
 import styles from '../styles/base-page.css'
 import tinyBackground from '../images/midday-flatirons-tiny.jpg'
-import largeBackground from '../images/midday-flatirons.jpg'
+import largeBackgroundImg from '../images/midday-flatirons.jpg'
 
 import React from 'react'
 
@@ -8,6 +8,9 @@ import classNames from '../utils/class_names';
 
 import Header from '../components/header'
 
+
+// the large background URL needs a leading slash and the small BG data URI can't have one.
+const largeBackground = `/${largeBackgroundImg}`;
 
 /*
   Closing over this so that transitioning between pages doesn't re-trigger the
@@ -32,10 +35,10 @@ class BasePage extends React.Component {
     */
     const preloadImg = new window.Image();
     preloadImg.addEventListener('load', () => {
-      preloadedBGImg = `/${largeBackground}`;
+      preloadedBGImg = largeBackground;
       this.setState({ preloaded: true });
     });
-    preloadImg.src = `/${largeBackground}`;
+    preloadImg.src = largeBackground;
   }
 
   render() {