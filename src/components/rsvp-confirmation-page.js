6063c5d52e971a0fddff25059c3f1fe28d767220
Tyler Benjamin <tyler.scott.benjamin@gmail.com>
2017-12-24 13:24:12 -0700

    Change Copy On RSVP Confirmation Page

--- src/components/rsvp-confirmation-page.js
+++ src/components/rsvp-confirmation-page.js
@@ -8,7 +8,7 @@ export default (props) => {
   return (
     <Base>
       <h1 className={styles.header}>Thank You!</h1>
-      <h2 className={styles.subheader}>Hope you can make it!</h2>
+      <h2 className={styles.subheader}>We treasure your friendship!</h2>
     </Base>
   )
 }
