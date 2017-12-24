56e48a2c870e3a9781f790c72ee8dc73817dc134
Tyler Benjamin <tyler.scott.benjamin@gmail.com>
2017-12-24 13:53:47 -0700

    Copy Tweak On RSVP Page

--- src/components/rsvp-page.js
+++ src/components/rsvp-page.js
@@ -10,6 +10,7 @@ export default (props) => {
     <Base>
       <h1 className={styles.header}>RSVP</h1>
       <h2 className={styles.subheader}>Can you make it?</h2>
+      <p className={styles.textBody}>We hope to celebrate with all of our close friends and family!</p>
       <RSVPForm />
     </Base>
   )
