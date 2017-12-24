ef2e3c03f637123aa7d0ec15982b9d9d2cfd7ab8
Tyler Benjamin <tyler.scott.benjamin@gmail.com>
2017-12-24 12:21:18 -0700

    Replace Lorem Ipsum On Home Page
    ... my kingdom for a copywriter.

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -37,15 +37,8 @@ export default (props) => {
         </span>
       </div>
 
-      <h2 className={styles.sectionHeader}>Our Story</h2>
       <p className={styles.bodyCopy}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Please help us celebrate our love back in the city where we first met.
       </p>
     </Base>
   )
