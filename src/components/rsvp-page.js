157892975134a10872f0e6e28d1d35b2ad38bdf9
Tyler Benjamin <tyler.scott.benjamin@gmail.com>
2020-01-31 19:12:18 -0800

    Update Copy On RSVP Page

--- src/components/rsvp-page.js
+++ src/components/rsvp-page.js
@@ -8,15 +8,8 @@ export default (props) => {
   return (
     <Base>
       <h1 className={styles.header}>It's Too Late</h1>
-      <h2 className={styles.subheader}>You can't come now.</h2>
-      <p>
-        <b>Just kidding!</b> If you're planning to attend and haven't already
-        told us, send an email to <a
-          className={styles.link}
-          href='mailto:rsvp@tylerandsarah.com'
-         >rsvp@tylerandsarah.com</a> containing a list of everyone in your
-         party and an apology for making us wait so long. ;)
-      </p>
+      <h2 className={styles.subheader}>You missed it.</h2>
+      <p>It was a lot of fun and you missed out. Sorry!</p>
     </Base>
   )
 }
